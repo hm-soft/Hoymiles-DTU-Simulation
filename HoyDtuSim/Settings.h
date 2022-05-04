6a5654daef87206bf7578e7e82522d1f23886eb8
hm-soft <77792275+hm-soft@users.noreply.github.com>
2022-05-04 15:03:33 +0200

    Add files via upload

--- HoyDtuSim/Settings.h
+++ HoyDtuSim/Settings.h
@@ -7,12 +7,6 @@
 
 #include "Debug.h"
 
-// Ausgabe was gesendet wird; 0 oder 1 
-#define DEBUG_SEND  0   
-
-// soll zwichen den Sendekanälen 23, 40, 61, 75 ständig gewechselt werden
-#define CHANNEL_HOP
-
 // mit OTA Support, also update der Firmware über WLan mittels IP/update
 #define WITH_OTA
 
@@ -32,11 +26,17 @@
 #define MAX_RF_PAYLOAD_SIZE     (32)
 #define DEFAULT_RF_DATARATE     (RF24_250KBPS)  // Datarate
 
+// Ausgabe was gesendet wird; 0 oder 1 
+#define DEBUG_SEND  1   
+
+// soll zwichen den Sendekanälen 23, 40, 61, 75 ständig gewechselt werden
+#define CHANNEL_HOP
+
 #define USE_POOR_MAN_CHANNEL_HOPPING_RCV  1     // 0 = not use
 
 #define DUMMY_RADIO_ID          ((uint64_t)0xDEADBEEF01ULL) 
 #define DTU_RADIO_ID            ((uint64_t)0x1234567801ULL)
-#define MAX_ANZ_INV 2             							// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
+#define MAX_ANZ_INV 2             // <<<<<< anpassen oder dyn.
 #define MAX_MEASURE_PER_INV 25    // hier statisch, könnte auch dynamisch erzeugt werden, aber Overhead für dyn. Speicher?
 
 // Webserver
@@ -44,26 +44,26 @@
 
 // Time Server
 //#define TIMESERVER_NAME "pool.ntp.org"
-#define TIMESERVER_NAME "fritz.box"							// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
+#define TIMESERVER_NAME "fritz.box"
 
 #ifdef WITH_OTA
 // OTA Einstellungen
 #define UPDATESERVER_PORT   WEBSERVER_PORT+1
 #define UPDATESERVER_DIR    "/update"
-#define UPDATESERVER_USER   "?????"							// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
-#define UPDATESERVER_PW     "?????"							// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
+#define UPDATESERVER_USER   "????"				// <<<<<< anpassen
+#define UPDATESERVER_PW     "????"				// <<<<<< anpassen
 #endif
 
 // internes WLan
 // PREFIXE dienen dazu, die eigenen WLans (wenn mehrere) von fremden zu unterscheiden
 // gehe hier davon aus, dass alle WLans das gleiche Passwort haben. Wenn nicht, dann mehre Passwörter hinterlegen
-#define SSID_PREFIX1         "pre1"							// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
-#define SSID_PREFIX2         "pre2"							// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
-#define SSID_PASSWORD        "?????????????????"			// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
+#define SSID_PREFIX1         "????"					// <<<<<< anpassen
+#define SSID_PREFIX2         "????"					// <<<<<< anpassen
+#define SSID_PASSWORD        "???????????????????"	// <<<<<< anpassen
 
 // zur Berechnung von Sonnenauf- und -untergang
-#define  geoBreite  49.2866									// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
-#define  geoLaenge  7.3416									// <<<<<<<<<<<<<<<<<<<<<<<< anpassen
+#define  geoBreite  49.2866							// <<<<<< anpassen
+#define  geoLaenge  7.3416							// <<<<<< anpassen
 
 
 #endif
